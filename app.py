0e22ad0fe613fb523ccb748fd9a05e22a985b37b
Vishal Salgond <vnsalgond@gmail.com>
2020-06-02 00:16:15 +0530

    second commit

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template, request
 from googleapiclient.discovery import build
 import os
 import json
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'AIzaSyA8hURwcnKk-2jGHgKuGshvJwfUgajyMT4'
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if (request.method == 'POST'):
-        youtube = build('youtube', 'v3', developerKey = os.environ.get('YT'))
+        youtube = build('youtube', 'v3', developerKey = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS'))
         data = dict()
         
         def get_channel_details(url):
