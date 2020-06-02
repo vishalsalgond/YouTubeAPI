939125b1b300b0c342360802291c223387c92f11
Vishal Salgond <vnsalgond@gmail.com>
2020-06-02 15:34:48 +0530

    speed changed

--- static/index.js
+++ static/index.js
@@ -1,14 +1,14 @@
 const counters = document.querySelectorAll('.counter');
-const speed = 50;
+const speed = 200;
 
 counters.forEach(counter => {
   const updateCount = () => {
     const target = counter.getAttribute('data-target');
-    const count = Math.floor(counter.innerText);
-    const inc = Math.floor(target / speed);
+    const count = Math.ceil(counter.innerText);
+    const inc = Math.ceil(target / speed);
     if (count < target) {
 
-      counter.innerText = Math.floor(count + inc);
+      counter.innerText = Math.ceil(count + inc);
       setTimeout(updateCount, 1);
     }
     else {
